3a4807d5b934d517c43f77efd22dd23322e044dc
Frost <aleksi.soini2004@gmail.com>
2021-11-06 17:49:35 +0200

    GUI improvements

--- Assets/Scripts/Photon/ConnectToServer.cs
+++ Assets/Scripts/Photon/ConnectToServer.cs
@@ -1,11 +1,17 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
+using TMPro;
+using System.Collections;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TMP_Text header;
+
     private void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        StartCoroutine(UpdateHeader());
+        StartCoroutine(Connect());
     }
 
     public override void OnConnectedToMaster()
@@ -16,5 +22,30 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         SceneManager.LoadScene("Lobby");
+    }
+
+    public IEnumerator UpdateHeader()
+    {
+        while (true)
+        {
+            header.text = "Connecting";
+            yield return new WaitForSeconds(0.5f);
+            header.text = "Connecting.";
+            yield return new WaitForSeconds(0.5f);
+            header.text = "Connecting..";
+            yield return new WaitForSeconds(0.5f);
+            header.text = "Connecting...";
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(UpdateHeader());
+        }
+    }
+
+    public IEnumerator Connect()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5.0f);
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 }
