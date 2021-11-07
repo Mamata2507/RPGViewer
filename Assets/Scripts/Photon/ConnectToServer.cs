e16d9be28424d9aaf0cf589c0de18d58073e4ee6
Frost <aleksi.soini2004@gmail.com>
2021-11-07 16:50:47 +0200

    added example map

--- Assets/Scripts/Photon/ConnectToServer.cs
+++ Assets/Scripts/Photon/ConnectToServer.cs
@@ -44,7 +44,7 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     {
         while (true)
         {
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(3.0f);
             PhotonNetwork.ConnectUsingSettings();
         }
     }
