0590bc11628a33d96e95934850788354dafe8b7d
Frost <aleksi.soini2004@gmail.com>
2021-11-22 00:05:12 +0200

    added AssetBundles

---
+++ Assets/Scripts/Editor/BundleBuilder.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles(@"C:\Projects\GitHub\Repositories\RPGViewer\AssetBundles", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+    }
+}
