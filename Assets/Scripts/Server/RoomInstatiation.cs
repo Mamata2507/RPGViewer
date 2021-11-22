cb2e658293ca7f28f24ad5ef3b0678e4c19b0b16
Frost <aleksi.soini2004@gmail.com>
2021-11-22 16:19:05 +0200

    updated AssetBundles

--- Assets/Scripts/Server/RoomInstatiation.cs
+++ Assets/Scripts/Server/RoomInstatiation.cs
@@ -5,11 +5,21 @@ using Photon.Pun;
 
 public class RoomInstatiation : MonoBehaviour
 {
+    private bool mapInstantiated = false;
+
     private void Start()
     {
         AssetBundles.GetAssetBundles("icons");
         AssetBundles.GetAssetBundles("maps");
+    }
 
-        PhotonNetwork.Instantiate(AssetBundles.GetItems("icons")[0].name, new Vector3(0, 0, 0), Quaternion.identity);
+    private void Update()
+    {
+        if (AssetBundles.GetItems("maps").Count > 0 && !mapInstantiated)
+        {
+            PreparePool.ReloadPrefabs();
+            mapInstantiated = true;
+            PhotonNetwork.Instantiate(AssetBundles.GetItems("maps")[0].name, new Vector3(0, 0, 0), Quaternion.identity);
+        }
     }
 }
