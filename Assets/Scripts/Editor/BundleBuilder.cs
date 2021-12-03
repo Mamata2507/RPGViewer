91374f7811760e4ddb571af1e71df2145e153aea
Frost <aleksi.soini2004@gmail.com>
2021-12-03 15:48:38 +0200

    added automatic transfer to Google Cloud

--- Assets/Scripts/Editor/BundleBuilder.cs
+++ Assets/Scripts/Editor/BundleBuilder.cs
@@ -7,5 +7,8 @@ public class BundleBuilder : Editor
     static void BuildAllAssetBundles()
     {
         BuildPipeline.BuildAssetBundles(@"C:\Projects\GitHub\Repositories\RPGViewer\AssetBundles", BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+        string init;
+        init = File.ReadAllText(@"C:\Projects\GitHub\Repositories\RPGViewer\AssetBundles\init.txt");
+        System.Diagnostics.Process.Start(@"C:\Users\ALEKSI\AppData\Local\Google\Cloud SDK\cloud_env.bat", init);
     }
 }
